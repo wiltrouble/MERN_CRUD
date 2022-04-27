29243899be1f3f61ec4237766511aa96bcaabbb7
Wilson Lopez <wilson.lopez@jalasoft.com>
2022-04-27 11:52:08 -0400

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import postsRoutes from './routes/post.routes.js';
+import { connectDB } from './db.js';
+import { PORT } from './config.js';
 
 const app = express()
+connectDB()
 app.use(postsRoutes)
 
-app.listen(3000)
-console.log('Server is running port', 3000);
+app.listen(PORT)
+console.log('Server is running port', PORT);
