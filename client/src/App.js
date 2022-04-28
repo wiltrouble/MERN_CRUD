61ba06a20764a8cebd3f393b1d5cd41a25efd848
Wilson Lopez <wilson.lopez@jalasoft.com>
2022-04-28 14:57:42 -0400

    react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { HomePage, PostForm } from './pages';
+import { HomePage, PostForm, NotFoundPage } from "./pages";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-    <HomePage />
-    <PostForm />
-    </>
-    
-  )
-}
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container bg-red-100 m-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new" element={<PostForm />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      </div>
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
